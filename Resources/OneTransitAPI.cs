cef41c98429c482d5ba7944b027684569e4bacbf
Matt McCormick <mbmccormick@gmail.com>
2011-09-19 17:11:26 -0300

    Added Consumer Key requirement.

--- Resources/OneTransitAPI.cs
+++ Resources/OneTransitAPI.cs
@@ -11,7 +11,7 @@ namespace _4sqtransit.Resources
         public static dynamic GetAgencies()
         {
             System.Net.WebClient client = new System.Net.WebClient();
-            var jsonResult = client.DownloadString("http://www.onetransitapi.com/v1/agencies/getList");
+            var jsonResult = client.DownloadString("http://www.onetransitapi.com/v1/agencies/getList?key=04a935ed-3d58-4310-8a37-3e497edfc051");
             var result = Json.Decode(jsonResult);
 
             return result.GetAgenciesResult;
@@ -20,7 +20,7 @@ namespace _4sqtransit.Resources
         public static dynamic GetStopsByLocation(string agencyid, double latitude, double longitude, double radius)
         {
             System.Net.WebClient client = new System.Net.WebClient();
-            var jsonResult = client.DownloadString("http://www.onetransitapi.com/v1/stops/getListByLocation?agency=" + agencyid + "&lat=" + latitude + "&lon=" + longitude + "&radius=" + radius);
+            var jsonResult = client.DownloadString("http://www.onetransitapi.com/v1/stops/getListByLocation?key=04a935ed-3d58-4310-8a37-3e497edfc051&agency=" + agencyid + "&lat=" + latitude + "&lon=" + longitude + "&radius=" + radius);
             var result = Json.Decode(jsonResult);
 
             return result.GetStopsByLocationResult;
@@ -29,7 +29,7 @@ namespace _4sqtransit.Resources
         public static dynamic GetStopTimes(string agencyid, string stopid)
         {
             System.Net.WebClient client = new System.Net.WebClient();
-            var jsonResult = client.DownloadString("http://www.onetransitapi.com/v1/stops/getTimes?agency=" + agencyid + "&stop=" + stopid);
+            var jsonResult = client.DownloadString("http://www.onetransitapi.com/v1/stops/getTimes?key=04a935ed-3d58-4310-8a37-3e497edfc051&agency=" + agencyid + "&stop=" + stopid);
             var result = Json.Decode(jsonResult);
 
             return result.GetStopTimesResult;
